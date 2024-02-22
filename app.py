632d97b8187c0739bb7eb671aadc81e02ecdde88
Griggs Hutaff <griggs333@gmail.com>
2024-02-22 18:29:58 -0500

    -Removed 1 class -Added method for data call

--- app.py
+++ app.py
@@ -17,15 +17,15 @@ app = Flask(__name__)
 class ZipCode(BaseModel):
     zip: Annotated[str, Field(strip_whitespace=True), Field(min_length=5), Field(max_length=10), Field(pattern=r'^\d{5}(-\d{4})?$')]
 
-class NoaaStationAPICall:
+class NoaaAPICall:
     def __init__(self):
-        self.datasetid = 'NORMAL_ANN'
+        self.datasetid = 'datasetid=NORMAL_ANN'
         self.locationid = ''
         self.datacategoryid = ''
-        self.datatypeid = ''
+        self.datatypeid = ['ANN-TMIN-PRBLST-T32FP20', 'ANN-TMIN-PRBLST-T32FP50', 'ANN-TMIN-PRBLST-T32FP80']
         self.extent = ''
-        self.startdate = "2010-01-01"
-        self.enddate = "2010-01-01"
+        self.startdate = "startdate=2010-01-01"
+        self.enddate = "enddate=2010-01-01"
         self.units = ''
         self.sortfield = ''
         self.sortorder = ''
@@ -35,6 +35,9 @@ class NoaaStationAPICall:
         self.lon = ''
         self.stationid = ''
         self.includemetadata = "includemetadata=false"
+        # self.spring_frost_twenty
+        # self.spring_frost_fifty
+        # self.spring_frost_eighty
 
     def zip_to_lat_lon(self):
         baseUrl = "https://www.ncei.noaa.gov/cdo-web/api/v2/stations?"
@@ -49,8 +52,7 @@ class NoaaStationAPICall:
 
         self.lat = response.json()["results"][0]["latitude"]
         self.lon = response.json()["results"][0]["longitude"]
-        # print(response.text)
-        # print(response.json()["results"][0])
+
         return self.lat, self.lon
 
     def lat_lon_to_stationid(self):
@@ -60,34 +62,32 @@ class NoaaStationAPICall:
             'token': 'lJXcatwedWjNNDfeJMcluRTMxgkhNxLd'
         }
         self.extent = "extent=" + str(float(self.lat)-0.5) + "," + str(float(self.lon)-0.5) + "," + str(float(self.lat)+0.5) + "," + str(float(self.lon)+0.5)
-        print(self.extent)
 
         url = baseUrl + self.datasetid + "&" + self.extent + "&" + self.limit
 
         response = requests.request("GET", url, headers=headers, data=payload)
 
-        # self.stationid = response[0]["id"]
-        # print(response.text)
-        print(response.json()["results"])
+        self.stationid = [response.json()["results"][x]["id"] for x in range(len(response.json()["results"]))]
 
         return self.stationid
 
+    def noaa_data_api_call(self):
+        baseUrl = "https://www.ncei.noaa.gov/cdo-web/api/v2/data?"
+        payload = {}
+        headers = {
+            'token': 'lJXcatwedWjNNDfeJMcluRTMxgkhNxLd'
+        }
 
+        url = baseUrl + self.datasetid + "&" + "datatypeid=" + self.datatypeid[0] + "&" + "stationid=" + self.stationid[0] + "&" + self.startdate + "&" + self.enddate
 
+        response = requests.request("GET", url, headers=headers, data=payload)
 
-class NoaaDataAPICall():
-    datasetid = ''
-    datatypeid =''
-    locationid = ''
-    stationid = ''
-    startdate = "2010-01-01"
-    enddate = "2010-01-01"
-    units = ''
-    sortfield = ''
-    sortorder = ''
-    limit = ''
-    offset = ''
-    includemetadata = "includemetadata=false"
+        print(type(response.json()["results"]), response.json()["results"])
+        parsed_results = [response.json()["results"][0]["datatype"], response.json()["results"][0]["value"]]
+        # parsed_results = [[response.json()["results"][x]["datatype"], response.json()["results"][x]["value"]] for x in range(len(response.json()["results"]))]
+
+        return parsed_results
+
 
 
 # def noaa_station_finder(zip):
@@ -119,12 +119,12 @@ class NoaaDataAPICall():
 @app.route('/zipcode', methods={'POST', 'GET'})
 @validate()
 def get_zip(query: ZipCode):
-    station = NoaaStationAPICall()
-    station.locationid='locationid=ZIP:' + query.zip
-    a = station.zip_to_lat_lon()
-    b = station.lat_lon_to_stationid()
-    print(a, b)
-    print(station.stationid)
+    noaa = NoaaAPICall()
+    noaa.locationid='locationid=ZIP:' + query.zip
+    a = noaa.zip_to_lat_lon()
+    b = noaa.lat_lon_to_stationid()
+    c = noaa.noaa_data_api_call()
+    print(a, b, c)
     return query
 
 
