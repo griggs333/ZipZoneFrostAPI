74c29508531bd5e93b0c725033e9394f55f764e5
Griggs Hutaff <griggs333@gmail.com>
2024-02-23 14:26:14 -0500

    - Value to Date conversion added

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 import numpy as np
 from flask import Flask, render_template, url_for, request, redirect
-# from flask_wtf import FlaskForm
-# from wtforms import validators, SelectField
-# import requests as req
-# from bs4 import BeautifulSoup
 import os
 from flask_pydantic import validate
 from pydantic import BaseModel, Field
@@ -18,75 +14,6 @@ app = Flask(__name__)
 
 class ZipCode(BaseModel):
     zip: Annotated[str, Field(strip_whitespace=True), Field(min_length=5), Field(max_length=10), Field(pattern=r'^\d{5}(-\d{4})?$')]
-
-# class NoaaAPICall:
-#     def __init__(self):
-#         self.datasetid = 'datasetid=NORMAL_ANN'
-#         self.locationid = ''
-#         self.datacategoryid = ''
-#         self.datatypeid = ['ANN-TMIN-PRBLST-T32FP20', 'ANN-TMIN-PRBLST-T32FP50', 'ANN-TMIN-PRBLST-T32FP80']
-#         self.extent = ''
-#         self.startdate = "startdate=2010-01-01"
-#         self.enddate = "enddate=2010-01-01"
-#         self.units = ''
-#         self.sortfield = ''
-#         self.sortorder = ''
-#         self.limit = 'limit=5'
-#         self.offset = ''
-#         self.lat = ''
-#         self.lon = ''
-#         self.stationid = ''
-#         self.includemetadata = "includemetadata=false"
-#
-#     def zip_to_lat_lon(self):
-#         baseUrl = "https://www.ncei.noaa.gov/cdo-web/api/v2/stations?"
-#         payload = {}
-#         headers = {
-#             'token': 'lJXcatwedWjNNDfeJMcluRTMxgkhNxLd'
-#         }
-#
-#         url = baseUrl + self.locationid + "&" + self.limit + "&" + self.includemetadata
-#
-#         response = requests.request("GET", url, headers=headers, data=payload)
-#
-#         self.lat = response.json()["results"][0]["latitude"]
-#         self.lon = response.json()["results"][0]["longitude"]
-#
-#         return self.lat, self.lon
-#
-#     def lat_lon_to_stationid(self):
-#         baseUrl = "https://www.ncei.noaa.gov/cdo-web/api/v2/stations?"
-#         payload = {}
-#         headers = {
-#             'token': 'lJXcatwedWjNNDfeJMcluRTMxgkhNxLd'
-#         }
-#         self.extent = "extent=" + str(float(self.lat)-0.5) + "," + str(float(self.lon)-0.5) + "," + str(float(self.lat)+0.5) + "," + str(float(self.lon)+0.5)
-#
-#         url = baseUrl + self.datasetid + "&" + self.extent + "&" + self.limit
-#
-#         response = requests.request("GET", url, headers=headers, data=payload)
-#
-#         self.stationid = [response.json()["results"][x]["id"] for x in range(len(response.json()["results"]))]
-#
-#         return self.stationid
-#
-#     def noaa_data_api_call(self):
-#         baseUrl = "https://www.ncei.noaa.gov/cdo-web/api/v2/data?"
-#         payload = {}
-#         headers = {
-#             'token': 'lJXcatwedWjNNDfeJMcluRTMxgkhNxLd'
-#         }
-#
-#         url = baseUrl + self.datasetid + "&" + "datatypeid=" + self.datatypeid[0] + "&" + "stationid=" + self.stationid[0] + "&" + self.startdate + "&" + self.enddate
-#
-#         response = requests.request("GET", url, headers=headers, data=payload)
-#
-#         print(type(response.json()["results"]), response.json()["results"])
-#         parsed_results = [response.json()["results"][0]["datatype"], response.json()["results"][0]["value"]]
-#         # parsed_results = [[response.json()["results"][x]["datatype"], response.json()["results"][x]["value"]] for x in range(len(response.json()["results"]))]
-#
-#         return parsed_results
-
 
 
 class NoaaAPICall:
@@ -120,13 +47,21 @@ class NoaaAPICall:
             parameter_list = self.__dict__.keys()
         i_dict = self.__dict__
         for i in parameter_list:
-            if type(i_dict[i]) is list:
+            if isinstance(i_dict[i], list):
                 for x in i_dict[i]:
                     query_string += str(i) + '=' + str(x) + "&"
             else:
                 query_string += str(i) + '=' + str(i_dict[i]) + "&"
 
         return query_string[:-1]
+
+    # NOAA API response crawler?
+    # def results_crawler(self, results, key_list, limit):
+    #     if isinstance(results, list):
+    #         for x in results:
+    #             for y in key_list:
+    #                 if y in
+    #     elif isinstance(results, dict):
 
     def zip_to_lat_lon(self):
         baseUrl = "https://www.ncei.noaa.gov/cdo-web/api/v2/stations?"
@@ -141,10 +76,6 @@ class NoaaAPICall:
 
         response = requests.request("GET", url, headers=headers, data=payload)
 
-        # print(response.text)
-        # print(response.json()["results"])
-        # print(response.json()["results"][0])
-
         self.lat = response.json()["results"][0]["latitude"]
         self.lon = response.json()["results"][0]["longitude"]
 
@@ -156,12 +87,12 @@ class NoaaAPICall:
         headers = {
             'token': 'lJXcatwedWjNNDfeJMcluRTMxgkhNxLd'
         }
-        # self.extent = "extent=" + str(float(self.lat)-0.5) + "," + str(float(self.lon)-0.5) + "," + str(float(self.lat)+0.5) + "," + str(float(self.lon)+0.5)
+        lat_bound_deg = 0.5
+        lon_bound_deg = 0.5
+        self.extent = str(float(self.lat)-lat_bound_deg) + "," + str(float(self.lon)-lon_bound_deg) + "," + str(float(self.lat)+lat_bound_deg) + "," + str(float(self.lon)+lon_bound_deg)
 
         query_string = self.queryBuilder(parameter_list=["datasetid", "extent", "limit", "includemetadata"])
         url = baseUrl + query_string
-
-        # print(url)
 
         response = requests.request("GET", url, headers=headers, data=payload)
 
@@ -195,6 +126,13 @@ class NoaaAPICall:
 
             return spring20, spring50, spring80
 
+        def value_to_date_conversion(value):
+            start_date = datetime.datetime.strptime("01/01", "%m/%d")
+            end_date = start_date + datetime.timedelta(days=value)
+
+            return end_date.strftime("%m/%d")
+
+
         baseUrl = "https://www.ncei.noaa.gov/cdo-web/api/v2/data?"
         payload = {}
         headers = {
@@ -204,20 +142,19 @@ class NoaaAPICall:
         query_string = self.queryBuilder(parameter_list=["datasetid", "datatypeid", "stationid", "startdate", "enddate", "includemetadata"])
         url = baseUrl + query_string
 
-        # print(url)
-
         response = requests.request("GET", url, headers=headers, data=payload)
-
-        # parsed_results = [response.json()["results"][0]["datatype"], response.json()["results"][0]["value"]]
 
         self.spring20, self.spring50, self.spring80 = append_spring_vals(response.json()["results"])
 
         # with open("response.json", "w") as f:
         #     json.dump(response.json()["results"], f)
+
         start_date = "01/01/24"
         date_1 = datetime.datetime.strptime(start_date, "%m/%d/%y")
 
-        return {"Spring 20% Value": average_list(self.spring20), "Spring 50% Value": average_list(self.spring50), "Spring 80% Value": average_list(self.spring80), "Spring20 date":date_1 + datetime.timedelta(days=average_list(self.spring20))}
+        return {"Spring 20% Date": value_to_date_conversion(value=average_list(self.spring20)),
+                "Spring 50% Date": value_to_date_conversion(value=average_list(self.spring50)),
+                "Spring 80% Date": value_to_date_conversion(value=average_list(self.spring80))}
 
 
 @app.route('/zipcode', methods={'POST', 'GET'})
@@ -229,14 +166,14 @@ def get_zip(query: ZipCode):
 
     station_call = NoaaAPICall()
     station_call.lat, station_call.lon = zip_call.zip_to_lat_lon()
-    station_call.extent = str(float(station_call.lat)-0.5) + "," + str(float(station_call.lon)-0.5) + "," + str(float(station_call.lat)+0.5) + "," + str(float(station_call.lon)+0.5)
+    # station_call.extent = str(float(station_call.lat)-0.5) + "," + str(float(station_call.lon)-0.5) + "," + str(float(station_call.lat)+0.5) + "," + str(float(station_call.lon)+0.5)
 
     data_call = NoaaAPICall()
     data_call.stationid = station_call.lat_lon_to_stationid()
-    data_call.datasetid = 'NORMAL_ANN'
-    data_call.datatypeid = ['ANN-TMIN-PRBLST-T32FP20', 'ANN-TMIN-PRBLST-T32FP50', 'ANN-TMIN-PRBLST-T32FP80']
-    data_call.startdate = "2010-01-01"
-    data_call.enddate = "2010-01-01"
+    # data_call.datasetid = 'NORMAL_ANN'
+    # data_call.datatypeid = ['ANN-TMIN-PRBLST-T32FP20', 'ANN-TMIN-PRBLST-T32FP50', 'ANN-TMIN-PRBLST-T32FP80']
+    # data_call.startdate = "2010-01-01"
+    # data_call.enddate = "2010-01-01"
 
     temp_result = data_call.noaa_data_api_call()
 
